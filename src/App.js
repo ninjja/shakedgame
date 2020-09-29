587f900e1f4e325eadbe21c7719ffc60a325e8ee
ninjja <ninja@outlook.co.il>
2020-09-29 11:08:19 +0300

    fix tries and failed message

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const ShakedGame = () => {
         if (started) {
             setClicked([]);
             setScore(0);
-            setTries(TRIES);
+            // setTries(TRIES);
 
             timer.current.start();
         }
@@ -87,14 +87,15 @@ const ShakedGame = () => {
     }
 
     const start = () => {
+      const _level = level+1;
         const _toys = allItems.map(x => ({ x, r: Math.random() }))
             .sort((a, b) => a.r - b.r)
             .map(a => a.x)
-            .slice(0, TOYS_COUNT(level+1));
+            .slice(0, TOYS_COUNT(_level));
 
         const _items = allItems.map((item) => {
-            const left = (Math.random() * (BOARD_SIZE(level+1) - ITEM_SIZE(level+1))).toFixed();
-            const top = (Math.random() * (BOARD_SIZE(level+1) - ITEM_SIZE(level+1))).toFixed();
+            const left = (Math.random() * (BOARD_SIZE(_level) - ITEM_SIZE(_level))).toFixed();
+            const top = (Math.random() * (BOARD_SIZE(_level) - ITEM_SIZE(_level))).toFixed();
 
             return (
               <Item onClick={() => handleItemClick(_toys, item)}
@@ -144,9 +145,9 @@ const ShakedGame = () => {
             {(started || score > 0 || tries < TRIES) && (<div className={classnames(['main-game'])}
                     style={{ minWidth: BOARD_SIZE(level), width: BOARD_SIZE(level), minHeight: BOARD_SIZE(level), height: BOARD_SIZE(level) }}>
 
-                    {score === 100 && <div className="hoorai">כל הכבוד הצלחת למצוא את הכל!</div>}
-                    {!started && score < 100 && tries > 0 && <div className="hoorai">כמעט! הצלחת למצוא {score}%, יפה מאוד.</div>}
-                    {!started && tries === 0 && <div className="hoorai">נגמרו לך הפסילות, מצאת {score}%.</div>}
+                    {score === 100 && <div className="hoorai">כל הכבוד הצלחת למצוא את הכל! <Button type="primary" onClick={() => start()}>לשלב הבא</Button></div>}
+                    {!started && score < 100 && tries > 0 && <div className="hoorai">כמעט! הגעת לשלב {level} והצלחת למצוא {score}%.</div>}
+                    {!started && tries === 0 && <div className="hoorai">נגמרו לך הפסילות, הגעת לשלב {level} ומצאת {score}%.</div>}
 
                     {items}
                 </div>)}
